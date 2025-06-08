f7d0458bf73cf7e2fe3b557a1246ca8a7442883c
JaceJung-dev <jacejung.dev@gmail.com>
2025-06-08 12:40:34 +0900

    ♻️ Refactor: Add exception

--- NATO_phonetic_alphabet/main.py
+++ NATO_phonetic_alphabet/main.py
@@ -7,6 +7,16 @@ df = pd.read_csv(f"{BASEDIR}/nato_phonetic_alphabet.csv")
 
 phonetic_alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-user_word = input("Type a word: ").upper()
-converted_word = [phonetic_alphabet_dict[char] for char in user_word]
-print(converted_word)
+
+def generate_phonetic():
+    user_word = input("Type a word: ").upper()
+    try:
+        converted_word = [phonetic_alphabet_dict[char] for char in user_word]
+    except KeyError:
+        print("Sorry, Only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(converted_word)
+
+
+generate_phonetic()
